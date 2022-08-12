dc51a62e9b5526173c0b1b786e21ea1c894867d7
Dary-Boyce <contact@darylboyce.com>
2022-08-12 20:35:28 +0100

    Added timmer functionality and background op.

--- index.php
+++ index.php
@@ -1,44 +1,54 @@
+
+<?php
+include ('php/head.php')
+?>
+<body>
+
 <?php
 // This calls the header files and uses them on the page //
     include ('php/header.php');
 ?>
-<body>
 
     <h2 id="product_header">montreal leather sofa</h2>
+
     <!-- Slideshow container -->
     <div class="slideshow-container">
-        <p id="countdown_timer"></p>
+        
+        <div class="timer_wrapper">
+        <span class='countdown_text_left'>order in the next</span>
+        <p id="countdown_timer"></p> 
+        <span class='countdown_text_right'>for next day delivery</span>
+        </div>
         <!-- Full-width images with number and caption text -->
         <div class="mySlides fade">
-        <img src="assets\sofa_tan.png" alt="tan sofa displayed with a table and mat" style="width:100%">
+        <img src="assets/sofa_tan.png" alt="tan sofa displayed with a table and mat" style="width:100%">
         </div>
 
         <div class="mySlides fade">
-        <img src="assets\sofa_tan.png" alt="tan sofa displayed with a table and mat" style="width:100%">
+        <img src="assets/sofa_tan.png" alt="tan sofa displayed with a table and mat" style="width:100%">
         </div>
 
         <div class="mySlides fade">
-        <img src="assets\sofa_tan.png" alt="tan sofa displayed with a table and mat" style="width:100%">
+        <img src="assets/sofa_tan.png" alt="tan sofa displayed with a table and mat" style="width:100%">
         </div>
 
         <div class="mySlides fade">
-        <img src="assets\sofa_tan.png" alt="tan sofa displayed with a table and mat" style="width:100%">
-        </div>
+        <img src="assets/sofa_tan.png" alt="tan sofa displayed with a table and mat" style="width:100%">
+        </div> 
 
-        <!-- Next and previous buttons -->
-        <a class="prev" onclick="plusSlides(-1)"><img src="assets\prev_arrow.png" alt="previous arrow on slider"></a>
-        <a class="next" onclick="plusSlides(1)" ><img src="assets\next_arrow.png" alt="next arrow on slider"></a>
+        <!--  Next and previous buttons  -->
+        <a class="prev" onclick="plusSlides(-1)"><img src="assets/prev_arrow.png" alt="previous arrow on slider"></a>
+        <a class="next" onclick="plusSlides(1)" ><img src="assets/next_arrow.png" alt="next arrow on slider"></a>
 
         <div class="slide_under">
-            <img class="slide_under_preview" src="assets\sofa_tan.png" onclick="currentSlide(1)">
+            <img class="slide_under_preview" src="assets/sofa_tan.png" onclick="currentSlide(1)" alt="preview of tan sofa under the slider">
 
-            <img class="slide_under_preview" src="assets\sofa_tan_option_2.png" onclick="currentSlide(2)">
+            <img class="slide_under_preview" src="assets/sofa_tan_option_2.png" onclick="currentSlide(2)" alt="preview of tan sofa under the slider">
 
-            <img class="slide_under_preview" src="assets\sofa_tan_option_3.png" onclick="currentSlide(3)">
+            <img class="slide_under_preview" src="assets/sofa_tan_option_3.png" onclick="currentSlide(3)" alt="preview of tan sofa under the slider">
 
-            <img class="slide_under_preview" src="assets\sofa_tan_option_4.png" onclick="currentSlide(4)">
-        </div>
-
+            <img class="slide_under_preview" src="assets/sofa_tan_option_4.png" onclick="currentSlide(4)" alt="preview of tan sofa under the slider">
+        </div> 
 
     </div>  
 
--- js/countdown_timer.js
+++ js/countdown_timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 15, 2024 15:37:25").getTime();
+var countDownDate = new Date("Aug 15, 2027 21:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -10,19 +10,22 @@ var x = setInterval(function() {
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  // Time calculations for hours, minutes and seconds
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+  function two_digits(n){
+    return (n < 10 ? "0" : "") + n;
+}
+
   // Display the result in the element with id="demo"
-  document.getElementById("countdown_timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown_timer").innerHTML = two_digits(hours) + ":" + two_digits(minutes) + ":" + two_digits(seconds);
 
   // If the count down is finishes write text to explain so
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown_timer").innerHTML = "EXPIRED";
   }
 }, 1000);
+
